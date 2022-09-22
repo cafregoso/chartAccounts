b89b6ccb09685880625e033826becbcac561f4ad
Carlos Alvarez <cafregoso@outlook.com>
2022-09-21 21:16:40 -0500

    adding apps for the API and the account model

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'api.apps.ApiConfig',
+    'account.apps.AccountConfig',
+
+    # 3rd Party Apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -44,7 +51,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'frontend/build'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '.static'
+STATICFILES_DIRS=[
+    'frontend/build/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
