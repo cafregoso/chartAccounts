b326cb175088c7ef605b230e2232e845364457b3
Carlos Alvarez <cafregoso@outlook.com>
2022-09-21 22:51:58 -0500

    pages to show the UI and consume the api

---
+++ frontend/src/services/services.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+class ChartDataService {
+    get_all() {
+        return axios.get('/api/charts/')
+    }
+
+    get_one(id) {
+        return axios.get(`/api/chart/${id}/`)
+    }
+
+    create_chart(data) {
+        return axios.post(`/api/chart/`, data)
+    }
+
+    update_chart(id, data) {
+        return axios.put(`/api/chart/${id}/`, data)
+    }
+
+    delete_chart(id) {
+        return axios.delete(`/api/chart/${id}/`)
+    }
+}
+
+export default new ChartDataService()
